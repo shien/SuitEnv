bb1d4ee5ab121f1552ed905186c9ebe7c2ce0837
TaikiTAIRA <sienrizumu@gmail.com>
2014-06-03 07:53:29 +0900

    can quit application, and fix little bug.

--- action.py
+++ action.py
@@ -10,10 +10,17 @@ class Action:
 
         self.mode_data = mode_data
 
+        self.modedic = {} 
+
+    def __close_apps(self, applist):
+        for appname in applist:
+            os.system("osascript -e 'tell application \"" + appname + "\" to quit'")
+
+
     # open apps list
     def __open_apps(self, applist):
         for appname in applist:
-            os.system('open -a' + appname)
+            os.system('open -a "' + appname + '"')
 
     # select mode from yaml file.
     def select_mode(self, event):
@@ -21,8 +28,37 @@ class Action:
         if modedic == "":
             return
 
-        apps = modedic['app']
-        self.__open_apps(apps)
+        if modedic == self.modedic:
+            return
+
+        if 'app' not in modedic:
+            return 
+
+        new_apps = modedic['app']
+
+        if 'app' not in self.modedic:
+            print("first exec")
+            self.__open_apps(new_apps)
+            self.modedic = modedic
+            return
+
+        old_apps = self.modedic['app']
+
+        # make to close apps list
+        set_close_app = set(old_apps) - set(new_apps)
+        close_apps = list(set_close_app)
+        print("close :")
+        print(close_apps)
+        self.__close_apps(close_apps)
+
+        # make to open apps list
+        set_open_apps = set(new_apps) - set(old_apps)
+        open_apps = list(set_open_apps)
+        print("open :")
+        print(open_apps)
+        self.__open_apps(open_apps)
+
+        self.modedic = modedic
 
     def __have_input_mode(self):
         if self.buf.get():
